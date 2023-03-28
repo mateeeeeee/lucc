63b7847593d0b3a2eccdf0202e5fa6d4f36870ec
Mate <mate.buljan1@gmail.com>
2023-03-29 00:19:54 +0200

    Add project files.

---
+++ Lumen/Core/Defines.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <cassert>
+
+#define _STRINGIFY_IMPL(a) #a
+#define _CONCAT_IMPL(x, y) x##y
+#define LU_STRINGIFY(a) _STRINGIFY_IMPL(a)
+#define LU_CONCAT(x, y) _CONCAT_IMPL( x, y )
+
+#define LU_ASSERT(expr) assert(expr)
+#define LU_ASSERT_MSG(expr, msg) assert(expr && msg)
+#define LU_OPTIMIZE_ON  #pragma optimize("", on)
+#define LU_OPTIMIZE_OFF #pragma optimize("", off)
+#define LU_WARNINGS_OFF #pragma(warning(push, 0))
+#define LU_WARNINGS_ON  #pragma(warning(pop))
+#define LU_DEBUGBREAK() __debugbreak()
+#define LU_FORCEINLINE	__forceinline
+#define LU_UNREACHABLE() __assume(false)
+
---
+++ Lumen/precomp.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <array>
+#include <stack>
+#include <memory>
+#include <span>
+#include <optional>
+#include <fstream>
+#include <functional>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <type_traits>
+#include <concepts>
+#include <any>
+#include <string_view>
+
