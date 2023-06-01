a0cc0077cd850791c2ead30d54396884515544ee
Mate <mate.buljan1@gmail.com>
2023-06-02 00:16:20 +0200

    moved assembly and linker to separate folder

--- Lucc/Compiler/Compiler.cpp
+++ Lucc/Compiler/Compiler.cpp
@@ -16,7 +16,9 @@ namespace lucc
 {
 	namespace
 	{
-		static constexpr char const* compiler_path = "Compiler/";
+		static constexpr char const* _compiler_path = "Compiler/";
+		static constexpr char const* _assembler_path = "Compiler/Assembler/";
+		static constexpr char const* _linker_path = "Compiler/Linker/";
 
 		std::string ExecuteSystemCmd(std::string const& _cmd)
 		{
@@ -39,7 +41,11 @@ namespace lucc
 	bool Compile(CompilerInput const& input)
 	{
 		diag::Initialize();
-		fs::current_path(fs::current_path() /= compiler_path);
+
+		fs::path compiler_path = fs::current_path() /= _compiler_path;
+		fs::path assembler_path = fs::current_path() /= _assembler_path;
+		fs::path linker_path = fs::current_path() /= _linker_path;
+		fs::current_path(compiler_path);
 
 		SourceBuffer src(input.source);
 		Lexer lex(src);
@@ -70,14 +76,25 @@ namespace lucc
 		
 		x86_64CodeGenerator x86_64(input.assembly, ast);
 		x86_64.Generate();
+		fs::rename(compiler_path/input.assembly, assembler_path/input.assembly);
 
+		fs::current_path(assembler_path);
 		std::string masm_cmd = std::format("ml64.exe /Fo {} /c {}", input.object, input.assembly);
-		std::string link_cmd = std::format("link.exe /out:{} {} /subsystem:console /entry:main", input.executable, input.object);
-		std::string exe_cmd  = std::format("{}", input.executable);
-
 		system(masm_cmd.c_str());
+		fs::current_path(compiler_path);
+		fs::rename(assembler_path/input.object, linker_path/input.object);
+
+		fs::current_path(linker_path);
+		std::string link_cmd = std::format("link.exe /out:{} {} /subsystem:console "
+			//libucrt.lib kernel32.lib 
+			//"/libpath:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.19041.0\\um\\x64"
+			//"/libpath:\\Program Files (x86)\\Windows Kits\\10\\Lib\\10.0.19041.0\\ucrt\\x64"
+			" /entry:main", input.executable, input.object);
 		system(link_cmd.c_str());
+
+		std::string exe_cmd = std::format("{}", input.executable);
 		system(exe_cmd.c_str());
+
 		return true;
 	}
 
