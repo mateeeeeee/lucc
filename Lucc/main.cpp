683e557c9bc4c1a062e2697534eb343c28a3e1e1
Mate <mate.buljan1@gmail.com>
2023-10-02 22:43:52 +0200

    misc

--- Lucc/main.cpp
+++ Lucc/main.cpp
@@ -3,34 +3,30 @@
 
 using namespace lucc;
 
-void PrintHelp();
-
 int main(int argc, char** argv)
 {
 	CLI::App cli_parser{ "lucc - little & useless C compiler" };
-	cli_parser.remove_option(cli_parser.get_help_ptr());
-	bool help = false;
-	cli_parser.add_flag("-h,--help", help);
-	CLI::Option* debug = cli_parser.add_flag("--debug");
-	CLI::Option* no_linking = cli_parser.add_flag("-c");
-	CLI::Option* no_assembling = cli_parser.add_flag("-S");
-	CLI::Option* only_preprocessor = cli_parser.add_flag("-E");
-	CLI::Option* ast_dump = cli_parser.add_flag("--astdump");
-	CLI::Option* dll = cli_parser.add_flag("--dll");
-	CLI::Option* lib = cli_parser.add_flag("--lib");
-	CLI::Option* test = cli_parser.add_flag("--test");
+	
+	CLI::Option* debug = cli_parser.add_flag("--debug", "Print debug information during compilation process");
+	CLI::Option* no_linking = cli_parser.add_flag("-c", "no linking is preformed, only .obj files are produced");
+	CLI::Option* no_assembling = cli_parser.add_flag("-S", "no assembling is preformed, only .asm files are produced");
+	CLI::Option* only_preprocessor = cli_parser.add_flag("-E", "Only preprocessor is run");
+	CLI::Option* ast_dump = cli_parser.add_flag("--astdump", "Print AST tree");
+	CLI::Option* dll = cli_parser.add_flag("--dll", "Build a dll file");
+	CLI::Option* lib = cli_parser.add_flag("--lib", "Build a lib file");
+	CLI::Option* test = cli_parser.add_flag("--test", "used for running g-tests");
 
 	std::vector<std::string> input_files;
-	CLI::Option* input_files_opt = cli_parser.add_option("-i", input_files, "Files to be compiled");
+	CLI::Option* input_files_opt = cli_parser.add_option("-i", input_files, "Input files");
 
 	std::string output_file;
-	CLI::Option* output_file_opt = cli_parser.add_option("-o", output_file);
+	CLI::Option* output_file_opt = cli_parser.add_option("-o", output_file, "Output file");
 	
 	std::string file_directory;
-	CLI::Option* file_directory_opt = cli_parser.add_option("-d", file_directory);
+	CLI::Option* file_directory_opt = cli_parser.add_option("-d", file_directory, "File directory of input files");
 	
 	std::string test_input;
-	CLI::Option* test_input_opt = cli_parser.add_option("-t", test_input);
+	CLI::Option* test_input_opt = cli_parser.add_option("-t", test_input, "input test code in form of a string");
 	{
 		dll->excludes(lib);
 		lib->excludes(dll);
@@ -40,12 +36,6 @@ int main(int argc, char** argv)
 		test_input_opt->needs(test);
 	}
 	CLI11_PARSE(cli_parser, argc, argv);
-
-	if (help)
-	{
-		PrintHelp();
-		return 0;
-	}
 
 	if (test)
 	{
@@ -69,24 +59,6 @@ int main(int argc, char** argv)
 	else if (lib) compiler_input.output_type = CompilerOutput::Lib;
 	else compiler_input.output_type = CompilerOutput::Exe;
 
-	int exit_code = Compile(compiler_input);
+	int32 exit_code = Compile(compiler_input);
 	return exit_code;
 }
-
-void PrintHelp()
-{
-	printf("The following options are available:\n");
-	printf("-h: for displaying available compile options\n");
-	printf("-c: no linking is preformed, only .obj files are produced\n");
-	printf("-S: no assembling is preformed, only .asm files are produced \n");
-	printf("-E: Only preprocessor is run\n");
-	printf("-d: Directory where the source files are located \n");
-	printf("-o: Output file \n");
-	printf("-i: Input files \n");
-	printf("--dll: builds a dll \n");
-	printf("--lib: builds a lib \n");
-	printf("--astdump: Dump AST to console\n");
-	printf("--test: used for running g-tests\n");
-	printf("--ti: input test code (used for g-tests)\n");
-}
-
