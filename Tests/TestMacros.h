d46fbc0461a322afba0246743efe2a017c7b1b02
Mate Buljan <mate.buljan1@gmail.com>
2023-10-17 22:27:17 +0200

    NDEBUG -> _DEBUG

--- Tests/TestMacros.h
+++ Tests/TestMacros.h
@@ -5,7 +5,7 @@
 #define LU_CODE(...) LU_STRINGIFY(__VA_ARGS__)
 #define LU_CODE_EX(...) LU_STRINGIFY(int main(void){__VA_ARGS__})
 
-#if NDEBUG
+#if _DEBUG
 
 #define LUCC_EX(...) system(LU_STRINGIFY(Luccd --test -t LU_CODE_EX(__VA_ARGS__)))
 #define LUCC_EX_DEBUG(...) system(LU_STRINGIFY(Luccd --test --debug -t LU_CODE_EX(__VA_ARGS__)))
